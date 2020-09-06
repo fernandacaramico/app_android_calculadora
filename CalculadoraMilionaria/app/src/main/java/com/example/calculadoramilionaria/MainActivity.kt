a1a484ecd41eb57560ff91e881e6803ab6dc7cdf
Fernanda Caramico <fernandacaramico@gmail.com>
2020-09-05 23:47:21 -0300

    add app feito pelo android studio

---
+++ CalculadoraMilionaria/app/src/main/java/com/example/calculadoramilionaria/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.calculadoramilionaria
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun calcular(componente:View) {
+        val proporcao = 0.55
+
+        var nome:String = et_texto_nome.text.toString()
+        var salario:Double = et_texto_valor_mes.text.toString().toDouble()
+        var gastoReal:Double = et_valor_essencial_mes.text.toString().toDouble()
+
+        var gastoIdeal = salario * proporcao
+        var valorAcima = gastoReal - gastoIdeal
+
+        if (nome.length < 3 || salario <= 0 || gastoReal <= 0) {
+            Toast.makeText(this, "Opa! Algo deu errado.", Toast.LENGTH_SHORT).show()
+
+            tv_resultado_texto.visibility = View.INVISIBLE
+            tv_resultado_texto_grande.visibility = View.INVISIBLE
+            tv_descricao_resultado.visibility = View.INVISIBLE
+
+            if (nome.length < 3) {
+                tv_red_nome.visibility = View.VISIBLE
+            } else {
+                tv_red_nome.visibility = View.INVISIBLE
+            }
+            if (salario <= 0) {
+                tv_red_salario.visibility = View.VISIBLE
+            } else {
+                tv_red_salario.visibility = View.INVISIBLE
+            }
+            if (gastoReal <= 0) {
+                tv_red_gastos.visibility = View.VISIBLE
+            } else {
+                tv_red_gastos.visibility = View.INVISIBLE
+            }
+        } else {
+            tv_red_nome.visibility = View.INVISIBLE
+            tv_red_salario.visibility = View.INVISIBLE
+            tv_red_gastos.visibility = View.INVISIBLE
+
+            tv_resultado_texto.visibility = View.VISIBLE
+            tv_resultado_texto_grande.visibility = View.VISIBLE
+            tv_descricao_resultado.visibility = View.VISIBLE
+
+            if (gastoReal > gastoIdeal) {
+                tv_resultado_texto_grande.text = "PERIGO!!"
+                tv_resultado_texto_grande.setTextColor(Color.RED)
+                tv_descricao_resultado.text = "$nome, seus gastos estão R$$valorAcima acima do ideal!! Que tal fazer um bazar com as blusinhas que não usa mais, hein, miga?"
+            } else if (gastoReal == gastoIdeal){
+                tv_resultado_texto_grande.text = "CUIDADO..."
+                tv_resultado_texto_grande.setTextColor(Color.parseColor("#ff8c00"))
+                tv_descricao_resultado.text = "$nome, seus gastos estão exatamente no valor ideal, parabéns pela disciplina. Mas cuidado, sair do controle pode ser mais fácil do que você imagina..."
+            } else {
+                tv_resultado_texto_grande.text = "SEGURA :)"
+                tv_resultado_texto_grande.setTextColor(Color.GREEN)
+                tv_descricao_resultado.text = "$nome, seus gastos estão dentro do ideal, parabéns! Você deve ser o exemplo da família, continue assim"
+            }
+        }
+
+    }
+
+    /*
+    *  fun clicaAqui(componente:View) {
+        Toast.makeText(this, "AI MACHUCOU", Toast.LENGTH_SHORT).show()
+
+        tv_topo.text="Opa! trocou!"
+        tv_segundo.text = et_texto.text
+
+        buuu.visibility = View.VISIBLE
+
+        tv_quarto.setTextColor(Color.CYAN)
+        tv_quarto.setTextColor(Color.parseColor("#ff00ff"))
+
+
+        val numeroInteiro = et_texto.text.toString().toInt()
+        val numeroReal = et_texto.text.toString().toDouble()
+
+        tv_segundo.text = "O dobro de $numeroInteiro é ${numeroInteiro*2}"
+
+    }
+    * */
+
+}
